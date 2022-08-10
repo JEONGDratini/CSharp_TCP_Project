864aa3b1bda123dbb1f76f76a8193c3b5420efee
aadevours73 <pw050503@naver.com>
2022-08-10 17:00:21 +0900

    코드에 주석추가

--- TCP_Async_Chatting_Server/TCP_Async_Chatting_Server/handleClient.cs
+++ TCP_Async_Chatting_Server/TCP_Async_Chatting_Server/handleClient.cs
@@ -13,56 +13,60 @@ namespace TCP_Async_Chatting_Server
     class handleClient
     {
 
+        //클라이언트 객체와 그것들을 닉네임과 맵핑시킨 딕셔너리 선언
         TcpClient clientSocket;
         public Dictionary<TcpClient, string> clientList = null;
 
         public void startClient(TcpClient ClientSocket, Dictionary<TcpClient, string> clientList)
         {
-            this.clientSocket = ClientSocket;
+            this.clientSocket = ClientSocket;//매개변수들을 각 속성에 집어넣는다.
             this.clientList = clientList;
 
-            Thread thread_handler = new Thread(Chat_execute);//새 스레드 생성해 실행.
+            Thread thread_handler = new Thread(Chat_execute);//새 스레드 언선
             thread_handler.IsBackground = true;//폼 종료되면 같이 종료되도록 설정.
-            thread_handler.Start();
+            thread_handler.Start();//스레드 실행
         }
 
-        public delegate void MessageDisplayHandler(string message, string NickName);//메시지 출력 핸들러 델리게이트 설정 및 선언
+        //메시지 출력 핸들러 델리게이트 설정 및 객체 선언
+        public delegate void MessageDisplayHandler(string message, string NickName);
         public event MessageDisplayHandler OnReceived;
 
-        public delegate void DisconnectedHandler(TcpClient clientSocket);//
+        //클라연결 종료 핸들러 델리게이트 설정 및 객체 선언
+        public delegate void DisconnectedHandler(TcpClient clientSocket);
         public event DisconnectedHandler OnDisconnected;
 
+        //채팅 실행.
         private void Chat_execute()
         {
             NetworkStream stream = null;
             try
             {
-                byte[] buffer = new byte[1024];
+                byte[] buffer = new byte[1024];//버퍼설정 (크기 : 1024바이트)
                 string msg = string.Empty;
                 int bytes = 0;
                 int MessageCount = 0;
 
-                while (true)
+                while (true)//계속반복한다.
                 {
                     MessageCount++;
-                    stream = clientSocket.GetStream();
-                    bytes = stream.Read(buffer, 0, buffer.Length);
+                    stream = clientSocket.GetStream();//클라소켓에서 스트림을 받아온다.
+                    bytes = stream.Read(buffer, 0, buffer.Length);//스트림에서 버퍼를 버퍼크기만큼 바이트 변수로 읽어온다.
                     msg = Encoding.Unicode.GetString(buffer, 0, bytes);
-                    msg = msg.Substring(0, msg.IndexOf("$"));
+                    msg = msg.Substring(0, msg.IndexOf("$"));//처음부터 $까지만 msg에 저장한다.
 
-                    if (OnReceived != null)
-                        OnReceived(msg, clientList[clientSocket].ToString());
+                    if (OnReceived != null)//처음에 만들었던 OnReceived 이벤트객체가 null이라면
+                        OnReceived(msg, clientList[clientSocket].ToString());//OnReceived메서드를 호출해준다.
                 }
             }
             catch 
             {
-                if (clientSocket != null)
+                if (clientSocket != null)//클라소켓이 null이 아닌데 뭔 문제가 생길 때 실행
                 {
-                    if (OnDisconnected != null)
-                        OnDisconnected(clientSocket);
+                    if (OnDisconnected != null)//처음에 만들었던 OnDisconnected 이벤트객체가 null이면
+                        OnDisconnected(clientSocket);//OnDisconnected메서드를 호출한다.
 
-                    clientSocket.Close();
-                    stream.Close();
+                    clientSocket.Close();//소켓닫고
+                    stream.Close();//네트워크 스트림도 닫는다.
                 }
 
             }
--- TCP_Chat_Client/TCP_Chat_Client/Form1.cs
+++ TCP_Chat_Client/TCP_Chat_Client/Form1.cs
@@ -14,7 +14,7 @@ namespace TCP_Chat_Client
 {
     public partial class Form1 : Form
     {
-        TcpClient clientSocket = new TcpClient(); //소켓
+        TcpClient clientSocket = new TcpClient(); //소켓 클라 생성
         NetworkStream stream = default(NetworkStream);
         string message = string.Empty;
 
@@ -23,47 +23,48 @@ namespace TCP_Chat_Client
             InitializeComponent();
         }
 
-        private void Connection_button_Click(object sender, EventArgs e)
+        private void Connection_button_Click(object sender, EventArgs e)//연결버튼 누르면
         {
             try
             {
-                clientSocket.Connect("111.111.0.31", 50000);
+                clientSocket.Connect("111.111.0.31", 50000);//연결 시도한다.
                 stream = clientSocket.GetStream();
             }
-            catch
+            catch//뭔 문제 생기면 실행.
             {
                 MessageBox.Show("서버 접속에 실패했습니다.");
                 Application.Exit();
             }
 
+            //catch안뜨고 잘 왔으면 실행
             message = "채팅 서버에 연결 되었습니다.";
             AddText(message);
 
-            byte[] buffer = Encoding.Unicode.GetBytes(textBox2.Text + "$");
-            stream.Write(buffer, 0, buffer.Length);
-            stream.Flush();
-            textBox2.ReadOnly = true;
+            byte[] buffer = Encoding.Unicode.GetBytes(textBox2.Text + "$");//닉네임 설정칸 내용 송신하고 해당 텍스트박스 비활성화
+            stream.Write(buffer, 0, buffer.Length);//버퍼에 담은 내용 스트림에 쓴다.
+            stream.Flush();//스트림 초기화
+            textBox2.ReadOnly = true;//textBox수정 못하게한다.
 
-            Thread tcp_handler = new Thread(GetMessage);
-            tcp_handler.IsBackground = true;
+            Thread tcp_handler = new Thread(GetMessage);//메시지 수신할 스레드 선언 및 시작
+            tcp_handler.IsBackground = true;//폼 종료되면 같이 종료되도록한다.
             tcp_handler.Start();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (textBox1.Text.Length > 0)
+            if (textBox1.Text.Length > 0)//아무것도 입력 안하고 전송하면 아무것도 전송 안하도록함.
             {
                 textBox1.Focus();
-                byte[] buffer = Encoding.Unicode.GetBytes(textBox1.Text + "$");
-                stream.Write(buffer, 0, buffer.Length);
-                stream.Flush();
-                textBox1.Text = "";
+                byte[] buffer = Encoding.Unicode.GetBytes(textBox1.Text + "$");//내용 버퍼에 집어넣고
+                stream.Write(buffer, 0, buffer.Length);//스트림에 쓰고
+                stream.Flush();//스트림 초기화하고
+                textBox1.Text = "";//보낼채팅 내용 쓰는 텍스트박스 clear시킨다.
             }
         }
 
         private void textBox1_KeyUP(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Enter)
+            if (e.KeyCode == Keys.Enter)//엔터키 입력받으면 텍스트박스 내용 송신한다.
                 button1_Click(this, e);
         }
 
@@ -75,29 +76,29 @@ namespace TCP_Chat_Client
                 stream.Write(buffer, 0, buffer.Length);
                 stream.Flush();
             }
-            Application.ExitThread();
-            Environment.Exit(0);
+            Application.ExitThread();//메인 스레드 종료하고
+            Environment.Exit(0);//프로세스 종료시킨다.
         }
 
-        private void GetMessage()
+        private void GetMessage()//메시지 수신받는 메서드
         {
-            while (true)
+            while (true)//계속반복한다.
             {
-                stream = clientSocket.GetStream();
-                int buffersize = clientSocket.ReceiveBufferSize;
-                byte[] buffer = new byte[buffersize];
+                stream = clientSocket.GetStream();//스트림받아와서
+                int buffersize = clientSocket.ReceiveBufferSize;//받은 버퍼사이즈를 int로 구하고
+                byte[] buffer = new byte[buffersize];//구한 사이즈만큼 버퍼 바이트배열 크기를 설정한다.
 
-                int bytes = stream.Read(buffer, 0, buffer.Length);
-                string message = Encoding.Unicode.GetString(buffer, 0, bytes);
-                AddText(message);
+                int bytes = stream.Read(buffer, 0, buffer.Length);//몇바이튼지 읽어오고
+                string message = Encoding.Unicode.GetString(buffer, 0, bytes);//string으로 바꾼다.
+                AddText(message);//그렇게 만든 메시지 출력.
             }
         }
 
-        private void AddText(string text)
+        private void AddText(string text)//채팅내용 텍스트박스 수정하는 메서드
         {
-            if (richTextBox1.InvokeRequired)
+            if (richTextBox1.InvokeRequired)//텍스트박스에 접근하는데 인보크가 필요하면 즉석 델리게이트 선언으로 실행한다.
                 richTextBox1.BeginInvoke(new MethodInvoker(delegate { richTextBox1.AppendText(text + "\r\n"); }));
-            else
+            else//인보크 필요없으면 걍 텍스트수정 갖다박는다.
                 richTextBox1.AppendText(text + "\r\n");
         }
 
